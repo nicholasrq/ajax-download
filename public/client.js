25ac233af910f6777dc6fbcc3451cd8af457e16b
Nikita Skryabin <nr@fenelon.ru>
2016-10-26 23:08:14 +0300

    awesomeness

--- public/client.js
+++ public/client.js
@@ -1,42 +1,106 @@
-// Upload
-const form  = document.querySelector('#upload'),
-      list  = document.querySelector('#files-list');
+const guid = function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
 
-const updateList = function(){
+const formatBytes = function(bytes, decimals) {
+   if(bytes == 0) return '0 Byte';
+   var k = 1000; // or 1024 for binary
+   var dm = decimals + 1 || 3;
+   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+   var i = Math.floor(Math.log(bytes) / Math.log(k));
+   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+const updateList = function(files){
+  const fragment = document.createDocumentFragment()
+
+  for(let file of files){
+    const li          = document.createElement('li'),
+          a           = document.createElement('a'),
+          progress    = document.createElement('progress');
+
+    a.textContent = file.name
+    a.href        = file.path
+    a.download    = file.name
+
+    progress.min    = 0
+    progress.max    = 100
+    progress.value  = 0
+    progress.hidden = true
+
+    li.appendChild(a)
+    li.appendChild(document.createTextNode(` ${formatBytes(file.size)}`))
+    li.appendChild(progress)
+    fragment.appendChild(li)
+  }
+
+  list.innerHTML = ""
+  list.appendChild(fragment)
+}
+
+const getFilesList = function(){
   const xhr = new XMLHttpRequest
 
   xhr.open('GET', '/list')
 
   xhr.onload = function(){
 
-    const response = JSON.parse(xhr.responseText),
-          fragment = document.createDocumentFragment()
+    const response = JSON.parse(xhr.responseText)
 
-    for(let file of response){
-      const li          = document.createElement('li'),
-            a           = document.createElement('a'),
-            progress    = document.createElement('progress');
-
-      a.textContent = file.name
-      a.href        = file.path
-      a.download    = file.name
-
-      progress.min    = 0
-      progress.max    = 100
-      progress.value  = 0
-      progress.hidden = true
-
-      li.appendChild(a)
-      li.appendChild(progress)
-      fragment.appendChild(li)
-    }
-
-    list.innerHTML = ""
-    list.appendChild(fragment)
+    updateList(response)
   }
 
   xhr.send()
 }
+
+const renderProgress = function(sessionID, ss){
+  for(let ssid in ss){
+    if(ssid == sessionID) continue;
+
+    const progress  = sessions.get(ssid),
+          session   = ss[ssid]
+
+    if(progress){
+      progress.value = session.progress
+
+      if(session.complete){
+        progress.parentNode.remove()
+        sessions.delete(ssid)
+      }
+    } else {
+      const progressElement = document.createElement('progress'),
+            fileName        = document.createTextNode(''),
+            wrapper         = document.createElement('li');
+
+      progressElement.min   = 0
+      progressElement.max   = 100
+      progressElement.value = session.progress
+
+      fileName.textContent = `${session.file} ${formatBytes(session.size)}`
+      
+      wrapper.appendChild(fileName)
+      wrapper.appendChild(progressElement)
+      progressFiles.appendChild(wrapper)
+      sessions.set(ssid, progressElement)
+    }
+  }
+}
+
+// Upload
+const form            = document.querySelector('#upload'),
+      list            = document.querySelector('#files-list'),
+      progressFiles   = document.querySelector('#progress'),
+      progressList    = document.querySelector('#progress-list');
+
+const ws        = new WebSocket(`ws://${location.host}`),
+      sessions  = new Map,
+      sessionID = guid();
 
 form.addEventListener('submit', function(e){
   e.preventDefault()
@@ -48,20 +112,36 @@ form.addEventListener('submit', function(e){
   button.disabled = true
   progress.hidden = false
 
-  const data  = new FormData(e.target),
-        xhr   = new XMLHttpRequest;
+  const data      = new FormData(e.target),
+        xhr       = new XMLHttpRequest,
+        file      = form.querySelector('input[type=file]').files[0];
 
   xhr.open('POST', '/upload')
 
   xhr.upload.onprogress = function(e){
     const percentage = e.loaded / e.total * 100
     progress.value = percentage
+
+    ws.send(JSON.stringify({
+      sessionID   : sessionID,
+      progress    : percentage,
+      file        : file.name,
+      size        : file.size,
+      complete    : false
+    }))
   }
 
   xhr.onload = function(){
     progress.hidden = true
     button.disabled = false
-    updateList()
+    ws.send(JSON.stringify({
+      sessionID   : sessionID,
+      progress    : 100,
+      file        : file.name,
+      size        : file.size,
+      complete    : true
+    }))
+    form.reset()
   }
 
   xhr.send(data)
@@ -112,4 +192,28 @@ list.addEventListener('click', function(e){
   }
 }, true)
 
-updateList()
+ws.addEventListener('message', e => {
+  const response = JSON.parse(e.data)
+  if(response.sessions){
+    const ss = Object.keys(response.sessions).map(k => response.sessions[k])
+    renderProgress(sessionID, response.sessions)
+
+    if(ss.filter(s => s.complete === false).length > 0){
+      progressList.hidden = false
+    } else {
+      progressList.hidden = true
+    }
+  } else if(response.connections){
+    document.querySelector('#connections').textContent = `${response.connections} active connections`
+  } else {
+    updateList(response)
+  }
+})
+
+ws.addEventListener('close', e => {
+  console.log('Connection closed')
+})
+
+window.onbeforeunload = () => { ws.close() }
+
+getFilesList()
--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-const express = require('express'),
-      http    = require('http'),
-      fs      = require('fs'),
-      fileUpload = require('express-fileupload');
+const express         = require('express'),
+      http            = require('http'),
+      fs              = require('fs'),
+      WebSocketServer = require('ws').Server,
+      fileUpload      = require('express-fileupload');
 
+const server    = http.createServer(),
+      app       = express(),
+      wss       = new WebSocketServer({ server }),
+      sessions  = {};
 
-const server  = http.createServer(),
-      app     = express();
+wss.broadcast = function broadcast(data) {
+  for(let client of wss.clients){
+    setTimeout(() => {
+      client.send(data);
+    })
+  }
+};
+
+const getFilesList = function(){
+  return new Promise(resolve => {
+    fs.readdir(`${__dirname}/upload`, function(err, files){
+      const filesListData = files.filter(file => {
+        return (/^\./.test(file)) === false
+      }).map(file => {
+        const filePath = `${__dirname}/upload/${file}`,
+              stat      = fs.statSync(filePath);
+        
+        return { name: file, path: `/files/${file}`, size: stat.size }
+      })
+
+      resolve(filesListData)
+    })
+  })
+}
 
 app.use(fileUpload())
 
@@ -14,13 +41,8 @@ app.use('/', express.static(__dirname + '/public'))
 app.use('/files', express.static(__dirname + '/upload'))
 
 app.get('/list', function(req, res){
-  fs.readdir(`${__dirname}/upload`, function(err, files){
-    res.status(200).send(files.map(file => {
-      const filePath = `${__dirname}/upload/${file}`,
-            stat      = fs.statSync(filePath);
-
-      return { name: file, path: `/files/${file}` }
-    }))
+  getFilesList().then(response => {
+    res.status(200).send(response)
   })
 })
 
@@ -37,13 +59,52 @@ app.post('/upload', function(req, res){
       if(err){
         res.status(200).send({error: err, success: false})
       } else {
-        res.status(200).send({error: null, success: true, file: `/files/${file.name}`})
+        const response = {error: null, success: true, file: `/files/${file.name}`}
+        res.status(200).send(response)
       }
     })
   }
 })
 
 server.on('request', app);
+
 server.listen(3000, function(){
   console.log('Example app listening on port 3000')
+})
+
+wss.on('connection', function(ws){
+  ws.on('message', message => {
+    const data = JSON.parse(message)
+
+    sessions[data.sessionID] = data
+    
+    for(let cli of wss.clients){
+      if(cli === ws) continue
+
+      cli.send(JSON.stringify({
+        sessions: sessions
+      }))
+
+      if(data.complete){
+        sessions[data.sessionID] = null
+        delete sessions[data.sessionID]
+      }
+    }
+  })
+
+  ws.on('close', function(){
+    wss.broadcast(JSON.stringify({
+      connections: wss.clients.length
+    }))
+  })
+
+  wss.broadcast(JSON.stringify({
+    connections: wss.clients.length
+  }))
+})
+
+fs.watch('./upload', function(){
+  getFilesList().then(response => {
+    wss.broadcast(JSON.stringify(response))
+  })
 })
